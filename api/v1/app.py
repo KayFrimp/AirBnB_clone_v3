713c2c179154df6232d5482c5265cbb42fe5ced2
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-10-28 22:55:46 -0700

    Updates to meet standardization

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""API nodule with Flask"""
+
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
 from os import getenv
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
@@ -21,8 +23,7 @@ def page_not_found(error):
 
 
 if __name__ == "__main__":
-
+    """Runs only when script is the main program"""
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     port = getenv('HBNB_API_PORT', default=5000)
-
     app.run(host, int(port), threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""init file for view module"""
+"""Blueprint Module for app_views"""
 
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-
 from api.v1.views.index import *
 from api.v1.views.states import *
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
