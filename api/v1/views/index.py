df8e7a18cf4e23a2b6e12948634d13446ae554ce
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-10-30 05:13:22 -0700

    Some stats working after few tweaks

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -12,12 +18,12 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def count():
     """an endpoint that retrieves the number of each objects by type"""
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
