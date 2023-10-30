b61caf34dc050a677c30467807abc06a13c29887
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-10-30 05:03:43 -0700

    Import error correction

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,8 @@
 """Blueprint Module for app_views"""
 
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+if app_views is not None:
+    from .index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,6 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-from flask import Flask
 
 
 @app_views.route('/status', strict_slashes=False)
