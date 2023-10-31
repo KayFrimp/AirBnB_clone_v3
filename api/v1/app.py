27ae35a048df6f20140188e81d71f403fb783685
Debby696 <idowuopeyemi696@gmail.com>
2023-10-30 18:00:54 -0700

    Update api/v1/app.py to create a CORS instance allowing: /* for 0.0.0.0

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""API nodule with Flask"""
+"""API module with Flask"""
 
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
